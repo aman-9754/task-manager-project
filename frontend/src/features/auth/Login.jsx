fd188b89b82968abea59d929a3904164dfcf217c
aman-9754 <amanfarkya143@gmail.com>
2026-04-02 10:04:40 +0530

    frontend -> Login.jsx done and i think works fine also

---
+++ frontend/src/features/auth/Login.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
+import { loginUser } from "../../api/userApi";
+
+const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await loginUser(form);
+
+      console.log("Login.jsx res:", res);
+
+      // const { accessToken, user } = res.data.data;
+      const { user } = res.data.data;
+
+      // store token
+      // localStorage.setItem("accessToken", accessToken);
+
+      // set user in context
+      setUser(user);
+
+      // redirect
+      navigate("/");
+    } catch (error) {
+      alert(error.response?.data?.message || "Login Failed!");
+    }
+  };
+
+  return (
+        <div className="flex items-center justify-center h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded shadow-md w-96"
+      >
+        <h2 className="text-xl font-bold mb-4">Login</h2>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="w-full mb-3 p-2 border"
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="w-full mb-3 p-2 border"
+          onChange={handleChange}
+          required
+        />
+
+        <button className="w-full bg-blue-500 text-white p-2">
+          Login
+        </button>
+
+        <p className="mt-3 text-sm">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-blue-500">
+            Register
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
